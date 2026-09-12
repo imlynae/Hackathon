010c560df968252d33c27697d675f97572569be8
imlynae <leehrnascimento@gmail.com>
2026-09-12 00:03:14 -0300

    Initial commit

---
+++ Source/Hackathon/Hackathon.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Hackathon : ModuleRules
+{
+	public Hackathon(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"Hackathon",
+			"Hackathon/Variant_Horror",
+			"Hackathon/Variant_Horror/UI",
+			"Hackathon/Variant_Shooter",
+			"Hackathon/Variant_Shooter/AI",
+			"Hackathon/Variant_Shooter/UI",
+			"Hackathon/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/Hackathon/Hackathon.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Hackathon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Hackathon, "Hackathon" );
+
+DEFINE_LOG_CATEGORY(LogHackathon)
